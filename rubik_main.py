7cff74cc1ad3a700ec824095ee265ba544ec8048
scottwmwork <scottwmwork@users.noreply.github.com>
2019-12-26 10:14:58 -0700

    Added comments and fixed formatting of cube list object

--- rubik_main.py
+++ rubik_main.py
@@ -3,41 +3,39 @@
 # Use multi-dimensional array to represent Cube
 cube = [[['b','b','b'],
          ['b','b','b'],
-         ['b','b','b']
-        ],
+         ['b','b','b']],
         [['r','r','r'],
          ['r','r','r'],
-         ['r','r','r']
-        ],
+         ['r','r','r']],
         [['g','g','g'],
          ['g','g','g'],
-         ['g','g','g']
-        ],
+         ['g','g','g']],
         [['w','w','w'],
          ['w','w','w'],
-         ['w','w','w']
-        ],
+         ['w','w','w']],
         [['y','y','y'],
          ['y','y','y'],
-         ['y','y','y']
-        ],
+         ['y','y','y']],
         [['o','o','o'],
          ['o','o','o'],
-         ['o','o','o'] 
-        ]]
+         ['o','o','o']]]
         
         
 # Functions for moving the cube
 def turnUp():
+    #TODO
     pass
 
 def turnDown():
+    #TODO
     pass
 
 def turnRight():
+    #TODO
     pass
 
 def turnLeft():
+    #TODO
     pass
 
 
