6936f7f4862bdb194fb3853084c79dd777ee5112
scottwmwork <scottwmwork@users.noreply.github.com>
2019-12-26 09:55:18 -0700

    Added rubik_main.py file that contains the cube array and a template for functions to fill

---
+++ rubik_main.py
@@ -0,0 +1,44 @@
+# Rubik's Cube Program
+
+# Use multi-dimensional array to represent Cube
+cube = [[['b','b','b'],
+         ['b','b','b'],
+         ['b','b','b']
+        ],
+        [['r','r','r'],
+         ['r','r','r'],
+         ['r','r','r']
+        ],
+        [['g','g','g'],
+         ['g','g','g'],
+         ['g','g','g']
+        ],
+        [['w','w','w'],
+         ['w','w','w'],
+         ['w','w','w']
+        ],
+        [['y','y','y'],
+         ['y','y','y'],
+         ['y','y','y']
+        ],
+        [['o','o','o'],
+         ['o','o','o'],
+         ['o','o','o'] 
+        ]]
+        
+        
+# Functions for moving the cube
+def turnUp():
+    pass
+
+def turnDown():
+    pass
+
+def turnRight():
+    pass
+
+def turnLeft():
+    pass
+
+
+
